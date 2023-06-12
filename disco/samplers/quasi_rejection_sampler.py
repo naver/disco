04d9d13b3e565838679fe2d575435aa503f22d4d
German[Natural Language Processing] <german.kruszewski@naverlabs.com>
2023-06-12 11:04:28 +0200

    added estimates of quantities for QRS
    * added estimates of important quantities for QRS
    * inverted the estimator's order of arguments to stay consistent with the QRS class

--- disco/samplers/quasi_rejection_sampler.py
+++ disco/samplers/quasi_rejection_sampler.py
@@ -5,7 +5,10 @@
 import torch
 
 from . import Sampler
+from .accumulation_sampler import AccumulationSampler
+from disco.metrics import KL
 from disco.utils.device import get_device
+from disco.utils.helpers import batchify
 
 
 class QuasiRejectionSampler(Sampler):
@@ -65,11 +68,132 @@ class QuasiRejectionSampler(Sampler):
         return accepted_samples, accepted_log_scores
 
     def get_acceptance_rate(self):
-        """Computes the acceptance rate, that is the number of accepted samples
-        over the total sampled ones
-        
+        """Computes the currently observed acceptance rate, that is the number
+        of accepted samples over the total sampled ones
+
         Returns
         -------
         acceptance rate as float between 0 and 1"""
 
         return self.n_accepted_samples / self.n_samples
+
+
+class QuasiRejectionSamplerEstimator:
+    """
+    Provides routines to compute estimates of useful metrics related to
+    QuasiRejectionSampler (QRS)
+    """
+
+    def __init__(self, target, proposal, n_estimation_samples=10000,
+            sampling_size=32, context=''):
+        """
+        Parameters
+        ----------
+        target: distribution
+            Energy-based model to (log-)score the samples
+        proposal: distribution
+            distribution to generate the samples
+        n_estimation_samples: integer
+            number of samples to use for computing estimates
+        sampling_size: integer
+            number of samples that are concurrently obtained from the proposal
+        context: text
+            context to condition the proposal and target
+        """
+        sampler = AccumulationSampler(proposal, total_size=n_estimation_samples)
+
+        self.samples, self.proposal_log_scores = \
+                sampler.sample(sampling_size=sampling_size, context=context)
+
+        self.target_log_scores = batchify(target.log_score,
+                sampling_size, samples=self.samples, context=context)
+
+    def acceptance_rate_at_beta(self, beta):
+        """
+        Estimate the acceptance rate that QRS has for a given beta parameter.
+
+        Parameters
+        ----------
+        beta: float
+            the value of beta for which we want the a.r. estimated
+
+        Returns
+        -------
+        the estimated acceptance rate for the given beta
+        """
+        vals = self._compute_intermediary_values(beta)
+
+        return vals['qrs_Z'].item() / beta
+
+    def divergence_at_beta(self, beta, divergence=KL):
+        """
+        Estimate the divergence to the target distribution that QRS has
+        for a given beta parameter.
+
+        Parameters
+        ----------
+        beta: float
+            the value of beta for which we want the a.r. estimated
+        divergence: divergence
+            the divergence that we want to compute (see :py:mod:`metrics`)
+
+        Returns
+        -------
+        the estimated value of the chosen divergence for the given beta
+        """
+        vals = self._compute_intermediary_values(beta)
+
+        return divergence.divergence(
+                vals['target_log_scores'],
+                vals['normalized_qrs_log_scores'],
+                vals['Z'],
+                vals['proposal_log_scores']).item()
+
+    def feature_moment_at_beta(self, beta, feature):
+        """
+        Estimate the first moment (expected value) of a feature when using
+        QRS with a given beta parameter.
+
+        Parameters
+        ----------
+        beta: float
+            the value of beta for which we want the a.r. estimated
+        feature: scorer
+            the feature whose moment we want to compute
+
+        Returns
+        -------
+        the estimated feature moment at the given beta
+        """
+        vals = self._compute_intermediary_values(beta)
+
+        feature_scores = batchify(feature.score,
+                sampling_size, samples=self.estimation_samples, context=context)
+
+        return torch.mean(vals['qrs_importance_ratios'] * feature_scores).item()
+
+    def _compute_intermediary_values(self, beta):
+        """
+        Computes intermediary values used in QRS estimates
+        """
+        with torch.no_grad():
+            target_importance_ratios = torch.exp(self.target_log_scores -
+                    self.proposal_log_scores)
+            Z = torch.mean(target_importance_ratios)
+            log_beta = torch.log(torch.tensor(beta)) if beta > 0 else float("-inf")
+            qrs_log_scores = torch.minimum(self.target_log_scores,
+                    self.proposal_log_scores + log_beta)
+            qrs_Z = torch.mean(torch.exp(qrs_log_scores - self.proposal_log_scores))
+            normalized_qrs_log_scores = qrs_log_scores - torch.log(qrs_Z)
+            qrs_importance_ratios = torch.exp(normalized_qrs_log_scores -
+                    self.proposal_log_scores)
+
+        return  {
+                'target_log_scores': self.target_log_scores,
+                'proposal_log_scores': self.proposal_log_scores,
+                'qrs_log_scores': qrs_log_scores,
+                'normalized_qrs_log_scores': normalized_qrs_log_scores,
+                'target_importance_ratios': target_importance_ratios,
+                'Z': Z,
+                'qrs_Z': qrs_Z,
+                'qrs_importance_ratios': qrs_importance_ratios}
--- tests/quasi_rejection_sampler_test.py
+++ tests/quasi_rejection_sampler_test.py
@@ -7,6 +7,10 @@ import unittest
 from disco.distributions import LMDistribution
 from disco.scorers import BooleanScorer
 from disco.samplers import QuasiRejectionSampler
+from disco.samplers.quasi_rejection_sampler import QuasiRejectionSamplerEstimator
+from disco.metrics import KL, TV
+from scipy import stats
+import torch
 
 class QuasiRejectionSamplerTest(unittest.TestCase):
 
@@ -25,6 +29,48 @@ class QuasiRejectionSamplerTest(unittest.TestCase):
         self.assertGreater(sampler.get_acceptance_rate(), 0.,
             "that sampling should probably return at least a sample.")
 
+    def test_estimator(self):
+        proposal = PoissonDistribution(10)
+        target = PoissonDistribution(11)
+        estimator = QuasiRejectionSamplerEstimator(target, proposal, n_estimation_samples=int(10e4))
+        ar = estimator.acceptance_rate_at_beta(0.5)
+        self.assertAlmostEqual(ar, 0.999, places=2)
+
+        ar = estimator.acceptance_rate_at_beta(1)
+        self.assertAlmostEqual(ar, 0.877, places=2)
+
+        ar = estimator.acceptance_rate_at_beta(2)
+        self.assertAlmostEqual(ar, 0.5, places=2)
+
+        tvd = estimator.divergence_at_beta(0.5, divergence=TV)
+        self.assertAlmostEqual(tvd, 0.12, places=2)
+
+        tvd = estimator.divergence_at_beta(1, divergence=TV)
+        self.assertAlmostEqual(tvd, 0.075, places=2)
+
+        tvd = estimator.divergence_at_beta(2, divergence=TV)
+        self.assertAlmostEqual(tvd, 0.0041, places=2)
+
+        kl = estimator.divergence_at_beta(1, divergence=KL)
+        self.assertAlmostEqual(kl, 0.021, places=2)
+
+        kl = estimator.divergence_at_beta(0.5, divergence=KL)
+        self.assertAlmostEqual(kl, 0.046, places=2)
+
+        kl = estimator.divergence_at_beta(2, divergence=KL)
+        self.assertAlmostEqual(kl, 0.0, places=2)
+
+class PoissonDistribution(object):
+
+    def __init__(self, lam):
+        self.lam = lam
+
+    def sample(self, sampling_size=1, context=None):
+        samples = list(stats.poisson.rvs(self.lam, size=sampling_size))
+        return samples, self.log_score(samples)
+
+    def log_score(self, x, context=None):
+        return torch.tensor(stats.poisson.logpmf(x, self.lam))
 
 if __name__ == '__main__':
     unittest.main()

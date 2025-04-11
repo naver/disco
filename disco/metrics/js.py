f6d818143b7a32b61640a59ce095c501bc97837e
German Kruszewski <germank@gmail.com>
2025-04-11 20:56:47 +0900

    robust computation of the JS metric

--- disco/metrics/js.py
+++ disco/metrics/js.py
@@ -11,8 +11,6 @@ from .kl import KL
 
 logger = logging.getLogger(__name__)
 
-MIN_EXPONENTIABLE_DOUBLE = -745
-
 class JS(BaseDivergence):
     """
     Jensen-Shannon divergence class.
@@ -39,17 +37,14 @@ class JS(BaseDivergence):
         divergence between m1 and m2
         """
 
-        very_small = lambda t: (MIN_EXPONENTIABLE_DOUBLE > t[~torch.isinf(t)]).any()
-
         device = get_device(m1_log_scores)
 
         m2_log_scores = m2_log_scores.to(device)
         normalized_m1_log_scores = m1_log_scores - torch.log(z)
 
-        if very_small(m1_log_scores) or very_small(m2_log_scores):
-            logger.warn(f"Scores below minimal precision in JS pointwise estimator")
+        max_log_scores = torch.max(normalized_m1_log_scores, m2_log_scores)
 
-        m_log_scores = torch.log((normalized_m1_log_scores.double().exp() + m2_log_scores.double().exp()) / 2).float()
+        m_log_scores = max_log_scores + torch.log(((normalized_m1_log_scores - max_log_scores).double().exp() + (m2_log_scores - max_log_scores).double().exp()) / 2).float()
 
         divergence = KL.pointwise_estimates(normalized_m1_log_scores, m_log_scores, torch.as_tensor(1), proposal_log_scores) / 2 + \
                 KL.pointwise_estimates(m2_log_scores, m_log_scores, torch.as_tensor(1), proposal_log_scores) / 2

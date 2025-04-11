Improve JS numerical stability with torch.log1p function and fixed variables nomenclature

disco/tuners/losses/js.py
@@ -6,6 +6,8 @@ from .base import BaseLoss
 import torch
 from .misc.ema_baseline import EMABaseline
 
+LOG_2 = torch.log(torch.tensor(2))
+
 class JSLoss(BaseLoss):
     """
     Jensen-Shannon divergence loss for DPG
@@ -24,7 +26,7 @@ class JSLoss(BaseLoss):
         if use_baseline:
             self.baseline = EMABaseline(ema_weight)
 
-    def __call__(self, samples, context, scores, ebm_scores, target_scores, z):
+    def __call__(self, samples, context, scores, ebm_scores, model_scores, z):
         """
         Computes the JS "loss" on a given minibatch of samples
 
@@ -38,8 +40,8 @@ class JSLoss(BaseLoss):
             logprobabilities for the samples according to the proposal
         ebm_scores: array of floats
             logscores for the samples according to the ebm
-        target_scores: array of floats
-            logprobabilities for the samples according to the target model
+        model_scores: array of floats
+            logprobabilities for the samples according to the model
         z: float
             estimation of the partition function of the EBM
  
@@ -48,16 +50,16 @@ class JSLoss(BaseLoss):
         mean loss across the minibatch
         """
 
-        importance_ratios = torch.exp(target_scores.detach() - scores)
+        importance_ratios = torch.exp(model_scores.detach() - scores)
         normalized_ebm_scores = ebm_scores - z.log()
-        rewards = importance_ratios * -torch.log((1 + (normalized_ebm_scores - target_scores.detach()).exp()) / 2)
+        rewards = importance_ratios * (-torch.log1p((normalized_ebm_scores - model_scores.detach()).exp()) + LOG_2)
         self.metric_updated.dispatch('importance_ratios', importance_ratios.mean())
         self.metric_updated.dispatch('rewards', rewards.mean())
         if self.use_baseline:
             advantage = self.baseline.advantage(rewards)
-            loss = torch.mean(advantage * target_scores)
+            loss = torch.mean(advantage * model_scores)
             self.metric_updated.dispatch('advantage', advantage.mean())
         else:
-            loss = torch.mean(rewards * target_scores)
+            loss = torch.mean(rewards * model_scores)
 
         return loss